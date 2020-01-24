d72dd73575e0c164038f4d661bfd6be8d84994c5
Josue <josue.rosario@solvex.com.do>
2020-01-24 11:32:17 -0400

    don desktop-home

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ $(window).scroll(function() {
     $(".navbar").removeClass("new-nav");
   }
 });
+
+$(document).ready(function() {
+  var $magic = $(".magic"),
+    magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function(e) {
+    if (!e.target.classList.contains("alfa")) {
+      $magic[0].style.display = "none";
+    } else {
+      $magic[0].style.display = "block";
+    }
+    $magic.css({ left: e.pageX - magicWHalf, top: e.pageY - magicWHalf });
+  });
+});
