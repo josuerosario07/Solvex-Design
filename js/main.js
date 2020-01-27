d23cc94abb0027a100ed751501caea9655ee0204
Josue <josue.rosario@solvex.com.do>
2020-01-27 16:54:33 -0400

    about section

--- js/main.js
+++ js/main.js
@@ -22,3 +22,26 @@ $(document).ready(function() {
     $magic.css({ left: e.pageX - magicWHalf, top: e.pageY - magicWHalf });
   });
 });
+
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+});
