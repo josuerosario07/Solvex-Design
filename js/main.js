ce855b773bff6e0a0316bbdcb9ef5553c7623df3
Josue <josue.rosario@solvex.com.do>
2020-01-31 11:30:16 -0400

    new header

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var rellax = new Rellax(".rellax");
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
 
-  if (scroll >= 200) {
+  if (scroll >= 5) {
     $(".navbar").addClass("new-nav");
   } else {
     $(".navbar").removeClass("new-nav");
